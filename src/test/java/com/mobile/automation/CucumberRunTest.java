eb1b9309e81d09371ebf30d8aed7bd2c7fda1aaa
LuLu <727084494@qq.com>
2022-06-09 22:36:23 +0800

    feat: cucumber for junit5
    1.add junit5 dependency
    2.Run cucumber entry point

---
+++ src/test/java/com/mobile/automation/CucumberRunTest.java
@@ -0,0 +1,19 @@
+package com.mobile.automation;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+
+/**
+ * @Author: Lulu
+ * @Description: 执行cucumber测试的入口
+ * @DateTime: 2022/6/9 19:22
+ **/
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("feature") // feature目录
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com/mobile/automation/steps") // step目录
+public class CucumberRunTest {}
